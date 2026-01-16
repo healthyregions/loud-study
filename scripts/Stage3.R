85572f1ae614d6989da49bf89732c6b6262085cc
Marynia <marynia.kolak@gmail.com>
2026-01-16 15:24:50 -0600

    finalized stage 3

--- scripts/Stage3.R
+++ scripts/Stage3.R
@@ -104,35 +104,77 @@ head(bupPol)
 bupPol.loud <- select(bupPol,HEROP_State,BupPolRst,BupPolRstSc)
 head(bupPol.loud)
 
+############
+# Merge stage 3 measures 
+############
+
+loud.stage3.1 <- merge(disability.loud, fqhc.loud, by="HEROP_ID")
+loud.stage3.2 <- merge(loud.stage3.1, commuting.loud, by="HEROP_ID")
+loud.stage3.3 <- merge(loud.stage3.2, moud.loud, by="HEROP_ID")
+loud.stage3.4 <- merge(loud.stage3.3, pharmacy.loud, by="HEROP_ID")
+
+head(loud.stage3.4)
+
+loud.stage3.4$HEROP_State <- str_sub(loud.stage3.4$HEROP_ID, 6,7)
+loud.stage3.4$HEROP_County <- str_sub(loud.stage3.4$HEROP_ID, 6,10)
+head(loud.stage3.4)
+
+loud.stage3.5 <- merge(loud.stage3.4, bupPol.loud, by="HEROP_State")
+loud.stage3.final <- merge(loud.stage3.5, od.mort.loud, by="HEROP_County")
+
+head(loud.stage3.final)
+
+## Merge with Geographic Boundaries, Continent only
+
+library(sf)
+tract.sf <- st_read("../indicators_raw/tract-continental.geojson")
+head(tract.sf)
+
+## Limit to US-continent only
+loud.stage3.us <- merge(tract.sf,loud.stage3.final, by="HEROP_ID")
+head(loud.stage3.us) #82613
+
+summary(loud.stage3.us)
+
+## Replace null driving times with -999 (=worse access vulnerable)
+## Only updating the "scaled" measure to preserve original data
+loud.stage3 <- loud.stage3.us %>%
+  mutate(across(c(FqhcTmDr2Sc, MetTmDr2Sc,
+                  BupTmDr2Sc,NaltTmDr2Sc,PharmTmDr2Sc), 
+                ~ replace_na(., -999)))
+
+summary(loud.stage3)
 
 ############
 # Calcs #
 ############
 
-## Transportation behaviors
-head(commuting)
+loud.stage3$DisbPScPPL <- percent_rank(loud.stage3$DisbPSc)
+loud.stage3$FqhcTmDr2ScPPL <- percent_rank(loud.stage3$FqhcTmDr2Sc)
+loud.stage3$NoVehHHldScPPL <- percent_rank(loud.stage3$NoVehHHldSc)
+loud.stage3$MetTmDr2ScPPL <- percent_rank(loud.stage3$MetTmDr2Sc)
+loud.stage3$BupTmDr2ScPPL <- percent_rank(loud.stage3$BupTmDr2Sc)
+loud.stage3$NaltTmDr2ScPPL <- percent_rank(loud.stage3$NaltTmDr2Sc)
+loud.stage3$PharmTmDr2ScPPL <- percent_rank(loud.stage3$PharmTmDr2Sc)
+loud.stage3$OdMortRtAvScPPL <- percent_rank(loud.stage3$OdMortRtAvSc)
+loud.stage3$BupPolRstScPPL <- percent_rank(loud.stage3$BupPolRstSc)
 
-commuting$NoVehHHldPPL <- percent_rank(commuting$NoVehHHldSc)
-commuting$CommTransitPPL <- percent_rank(commuting$CommTransitSc)
-commuting$CommWalkingPPL <- percent_rank(commuting$CommWalkingSc)
-
-head(commuting.loud)
-
-## Disability
-
-
-
-## MOUD Spatial Availability
+head(loud.stage3)
 
 
-## Pharmacy Availabiltiy
+# Equally Weighted
+loud.stage3$Stage3 <- (loud.stage3$DisbPScPPL + loud.stage3$FqhcTmDr2ScPPL +
+                             loud.stage3$NoVehHHldScPPL + loud.stage3$MetTmDr2ScPPL + 
+                             loud.stage3$BupTmDr2ScPPL + loud.stage3$NaltTmDr2ScPPL +
+                             loud.stage3$PharmTmDr2ScPPL + loud.stage3$OdMortRtAvScPPL + 
+                             loud.stage3$BupPolRstScPPL 
+                           )/9
+hist(loud.stage3$Stage3)
+head(loud.stage3)
 
+st_write(loud.stage3, "../data_final/loud.stage3.geojson")
 
-## FQHC
+#save(loud.stage5.df2,  file = "../data_final/loud_stage4-5.RData")
+loud.stage3.df <- st_drop_geometry(loud.stage3)
 
-
-## OUD Overdose Rates
-
-
-## State bup policies
-
+write.csv(loud.stage3.df, "../data_final/loud_stage3.csv", row.names = FALSE)
